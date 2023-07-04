3947e4e18a74eca6cda6b4a173675a6e75c3c423
rehansohan <rehansohan5303@gmail.com>
2023-07-04 23:37:44 +0600

    week-4

--- Week-3/Day-2/3-B.All Distinct/All Distinc.cpp
+++ Week-3/Day-2/3-B.All Distinct/All Distinc.cpp
@@ -1,1 +1,33 @@
 
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    int t;
+    cin >> t;
+while(t--)
+{
+ int n;
+ cin>>n;
+ vector<int>v(n);
+  map<int,int>mp;
+ for(int i=0;i<n;i++)
+ {
+  cin>>v[i];
+  mp[v[i]]++;
+ }
+int ans=mp.size();
+if((n-ans)%2!=0)
+ ans--;
+ cout<<ans<<endl;
+
+}
+
+
+    return 0;
+}
--- Week-3/Day-2/4-C.Most Similar Words/simillarword.cpp
+++ Week-3/Day-2/4-C.Most Similar Words/simillarword.cpp
@@ -1,1 +1,49 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+int diff(string a,string b)
+{
+ int c=0;
+ for(int i=0;i<a.size();i++)
+ {
+  c+=abs((a[i]-'0')-(b[i]-'0'));
+ }
+ return c;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+while(t--)
+{
+ int n,b;
+ cin>>n>>b;
+
+vector<string>s(n);
+for(int i=0;i<n;i++)
+{
+ cin>>s[i];
+
+}
+
+
+
+int ans=INT_MAX;
+for(int i=0;i<s.size();i++)
+{
+ for(int j=i+1;j<s.size();j++){
+
+ans=min(ans,diff(s[i],s[j]));
+ }
+
+}
+cout<<ans<<endl;
+
+
+
+}
+
+
+    return 0;
+}

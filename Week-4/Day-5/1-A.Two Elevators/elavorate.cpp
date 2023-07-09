4d2da5417dae1e3b99ef3d78c7af2219e4319f12
rehansohan <rehansohan5303@gmail.com>
2023-07-09 20:51:05 +0600

    Day-5

--- Week-4/Day-5/1-A.Two Elevators/elavorate.cpp
+++ Week-4/Day-5/1-A.Two Elevators/elavorate.cpp
@@ -1,1 +1,21 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+     int a,b,c;
+     cin>>a>>b>>c;
+     a=a-1;
+     b=abs(b-c)+(c-1);
+     if(a<b)cout<<"1"<<endl;
+     else if(a>b)cout<<"2"<<endl;
+     else cout<<"3"<<endl;
+    }
+    return 0;
+}

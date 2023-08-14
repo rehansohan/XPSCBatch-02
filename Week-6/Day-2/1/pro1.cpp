73417ab0e0ef9e3060a38d42402ee2fd7ff5aa8f
rehansohan <rehansohan5303@gmail.com>
2023-08-14 22:22:05 +0600

    Day-2

--- Week-6/Day-2/1/pro1.cpp
+++ Week-6/Day-2/1/pro1.cpp
@@ -1,1 +1,11 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin>>a>>b>>c;
+	int t=a+(c-1)*b;
+	cout<<t;
+	return 0;
+}
--- Week-6/Day-2/2/pro2.cpp
+++ Week-6/Day-2/2/pro2.cpp
@@ -1,1 +1,22 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    int x;
+	    cin>>x;
+	    if(x>=1&&x<=10)
+	    cout<<"Lower Double"<<endl;
+	    else if(x>=11&&x<=15)
+	    cout<<"Lower Single"<<endl;
+	    else if(x>=16&&x<=25)
+	    cout<<"Upper Double"<<endl;
+	    else
+	    cout<<"Upper Single"<<endl;
+	}
+	return 0;
+}

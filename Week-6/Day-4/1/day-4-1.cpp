db5d6aa9c813216f5a12ee2358c1d52ab5c58377
rehansohan <rehansohan5303@gmail.com>
2023-08-22 19:42:59 +0600

    Day-5

--- Week-6/Day-4/1/day-4-1.cpp
+++ Week-6/Day-4/1/day-4-1.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--)
+{
+    int a,b;
+    cin>>a>>b;
+    int k=b*2;
+    if(k>=a)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
+	return 0;
+}

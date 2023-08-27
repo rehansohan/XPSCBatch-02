376c0ead28f65bf63a8cb4aebd91b0b2c0f0e965
rehansohan <rehansohan5303@gmail.com>
2023-08-27 20:17:16 +0600

    day-1

---
+++ Week-7/Day-1/1/code-1.cpp
@@ -0,0 +1,46 @@
+
+//BISMILLAHIR RAHMANIR RAHEEM
+//ALLAH IS WATCHING ME
+//EMON HOSSEN SOHAN
+// Dept of ICE, NSTU
+#include<bits/stdc++.h>
+using namespace std;
+/*int gcd(int a,int b)
+{
+ if(b==0)
+  return a;
+ return gcd(b,a%b);
+}
+
+//type of sieve algorithom not sieve 2  4 8 16 32 or 3 6 12 24
+ vector<int>a(n+1,0);
+
+ for(int i=1;i<=n;i++)
+ {
+  if(a[i]==0){
+  for(int j=i;j<=n;j+=j)
+  {
+   a[j]++;
+   v.push_back(j);
+  }
+  }*/
+using ll = long long;
+using pii= pair<int,int>;
+
+const int N=200005;
+ll mod= 1e9+7;
+
+int main()
+{
+int a,b;
+cin>>a>>b;
+int s=(a^b);
+cout<<s<<endl;
+
+    return 0;
+}
+
+
+
+///  Alhamdulillah...
+
